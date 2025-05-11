4c83560e20c94d891060f8f6e810b8e228747861
MedSveta <sveta1978medved@gmail.com>
2025-05-11 19:30:13 +0300

    Start project

--- src/main/java/dto/UserDtoLombok.java
+++ src/main/java/dto/UserDtoLombok.java
@@ -1,4 +1,17 @@
 package dto;
 
+import lombok.*;
+
+@ToString
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class UserDtoLombok {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
 }
